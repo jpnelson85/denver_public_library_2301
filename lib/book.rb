33a961d3b89344c91e71bfeb4da8ccf2d9e77f96
Jeff Nelson <jpnelson85@gmail.com>
2023-02-24 10:22:01 -0600

    finish iteration 1

--- lib/book.rb
+++ lib/book.rb
@@ -2,14 +2,20 @@ class Book
   attr_reader :author_first_name,
               :author_last_name,
               :title,
-              :publication_date,
-              
+              :publication_date
 
   def initialize(details)
     @author_first_name = details[:author_first_name]
     @author_last_name = details[:author_last_name]
     @title = details[:title]
     @publication_date = details[:publication_date]
+  end
 
+  def author
+    "#{author_first_name} #{author_last_name}"
+  end
+
+  def publication_year
+    @publication_date[-4..-1]
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -15,6 +15,6 @@ RSpec.describe Book do
 
   it 'publication year of the book' do
     book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
-    expect(book.publication_date).to eq("1960")
+    expect(book.publication_year).to eq("1960")
   end
 end
