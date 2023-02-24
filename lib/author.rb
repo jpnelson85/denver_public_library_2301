6c3d04adfbdc38da3a2495351e42be7175b28a37
Jeff Nelson <jpnelson85@gmail.com>
2023-02-24 10:46:12 -0600

    write method

--- lib/author.rb
+++ lib/author.rb
@@ -13,5 +13,8 @@ class Author
     "#{first_name} #{last_name}"
   end
 
-  
+  def write(title, publication_date)
+    book = Book.new({title: title, publication_date: publication_date})
+    @books << book
+  end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -17,8 +17,14 @@ RSpec.describe Author do
   it 'write method' do
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-    expect(jane_eyre.class).to eq(Book)
+    expect(jane_eyre.class).to be_a(Book)
     expect(jane_eyre.title).to eq("Jane Eyre")
   end
 
+  it 'adds book to books array' do
+    author.write('Jane Eyre', 'October 16, 1847')
+    expect(author.books.size).to eq(1)
+    author.write('Villette', '1853')
+    expect(author.books.size).to eq(2)
+  end
 end
