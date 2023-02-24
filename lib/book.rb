8b93d1e06abc2c15ea71bba920c964721b3c4284
Jeff Nelson <jpnelson85@gmail.com>
2023-02-24 10:15:20 -0600

    update spec tests

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,15 @@
 class Book
-  attr_reader
+  attr_reader :author_first_name,
+              :author_last_name,
+              :title,
+              :publication_date,
+              
 
   def initialize(details)
     @author_first_name = details[:author_first_name]
     @author_last_name = details[:author_last_name]
     @title = details[:title]
-    
+    @publication_date = details[:publication_date]
+
   end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe Book do
     expect(book).to be_a(Book)
   end
 
-  
+  it 'title and author are attributes' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    expect(book.title).to eq("To Kill a Mockingbird")
+    expect(book.author).to eq("Harper Lee")
+  end
+
+  it 'publication year of the book' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+    expect(book.publication_date).to eq("1960")
+  end
 end
