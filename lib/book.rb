06f90d3eb030294f4afba00b36eb92413387ee54
Jeff Nelson <jpnelson85@gmail.com>
2023-02-24 10:08:08 -0600

    update book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,3 +1,10 @@
 class Book
-  
+  attr_reader
+
+  def initialize(details)
+    @author_first_name = details[:author_first_name]
+    @author_last_name = details[:author_last_name]
+    @title = details[:title]
+    
+  end
 end
